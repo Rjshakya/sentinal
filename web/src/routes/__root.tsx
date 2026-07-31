bb63777aca0da4bcd47de5fe0dc8bd9d9bac377a
Rjshakya <rajkamalshakya07@gmail.com>
2026-08-01 03:27:23 +0530

    fix title

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -20,7 +20,7 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "Review - Pr",
+        title: "reviewpr",
       },
     ],
     links: [
